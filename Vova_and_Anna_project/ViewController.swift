ad70e223c483af7ee2df6bea80f2bc08ea55090d
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-01-11 17:21:20 +0300

    Initial Commit

---
+++ Vova_and_Anna_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vova_and_Anna_project
+//
+//  Created by Vladimir Syleimanov on 11.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
